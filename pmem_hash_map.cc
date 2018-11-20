81cdfb56bbcaf3f537da7a7cf662273681e08473
HustCoderHu <coderhzx@gmail.com>
2018-11-20 16:41:05 +0800

    rename cpp to cc

--- pmem_hash_map.cc
+++ pmem_hash_map.cc
@@ -45,44 +45,12 @@ p_node pmem_hash_map::getNode(uint64_t hash, const std::string &key)
 
 uint64_t pmem_hash_map::put(pool_base &pop, const std::string &prefix,
                             size_t bufSize) {
-  // ( const void * key, int len, unsigned int seed );
-  uint64_t _hash = CityHash64WithSeed(prefix, prefix.length(), 16);
-
-  p_node nod = tab[_hash % tabLen];
-
-  p_node tmp = nod;
-  while (tmp != nullptr) {
-    if (tmp->hash_ == _hash
-        && strcmp(tmp->prefix_, prefix.c_str()) == 0)
-      break;
-    tmp = tmp->next;
-  }
-
-  // 前缀没有被插入过
-  if (nullptr == tmp) {
-    transaction::run(pop, [&] {
-      p_node newhead = make_persistent<p_node>();
-
-      newhead->hash_ = _hash;
-      newhead->prefixLen = prefix.length();
-      newhead->prefix_ = make_persistent<char[]>(prefix.length()+1);
-      memcpy(newhead->prefix_, prefix.c_str(), prefix.length()+1);
-
-      newhead->bufSize = bufSize
-      newhead->buf = make_persistent<char[]>(bufSize);
-
-      newhead->next = nod;
-      tab[_hash % tabLen] = newhead;
-    });
-  }
-  else {
-    // 已经插入过了
-  }
-  return _hash;
+  return putAndGet(pop, prefix, bufSize)->hash_;
 }
 
 p_node pmem_hash_map::putAndGet(pool_base &pop, const std::string &prefix, size_t bufSize)
 {
+  // ( const void * key, int len, unsigned int seed );
   uint64_t _hash = CityHash64WithSeed(prefix, prefix.length(), 16);
 
   p_node bucketHeadNode = tab[_hash % tabLen];
@@ -106,8 +74,16 @@ p_node pmem_hash_map::putAndGet(pool_base &pop, const std::string &prefix, size_
       newhead->prefix_ = make_persistent<char[]>(prefix.length()+1);
       memcpy(newhead->prefix_, prefix.c_str(), prefix.length()+1);
 
+      // TODO
+      // key_range_ ?
+      // seq_num_ ?
+      newhead->key_range_ = make_persistent<char[]>(?);
+      newhead->chunk_num_ = 0;
+      newhead->seq_num_ = ?;
+
       newhead->bufSize = bufSize
       newhead->buf = make_persistent<char[]>(bufSize);
+      newhead->dataLen = 0;
 
       newhead->next = bucketHeadNode;
       tab[_hash % tabLen] = newhead;
