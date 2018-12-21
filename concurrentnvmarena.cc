3e9acdd27563e4b85ec2a5198724c8c7962b402f
HustCoderHu <coderhzx@gmail.com>
2018-12-21 11:32:39 +0800

    nvm arena & concurrent

---
+++ concurrentnvmarena.cc
@@ -0,0 +1,159 @@
+#include "concurrentnvmarena.h"
+
+namespace rocksdb {
+
+namespace {
+// If the shard block size is too large, in the worst case, every core
+// allocates a block without populate it. If the shared block size is
+// 1MB, 64 cores will quickly allocate 64MB, and may quickly trigger a
+// flush. Cap the size instead.
+const size_t kMaxShardBlockSize = size_t{128 * 1024};
+}  // namespace
+
+ConcurrentNVMArena::ConcurrentNVMArena(size_t block_size, AllocTracker *tracker, size_t huge_page_size)
+    : shard_block_size_(std::min(kMaxShardBlockSize, block_size / 8)),
+      shards_(),
+      arena_(block_size, tracker, huge_page_size)
+{
+
+}
+
+size_t ConcurrentNVMArena::ShardAllocatedAndUnused() const
+{
+    size_t total = 0;
+    for (size_t i = 0; i < shards_.Size(); ++i) {
+        total += shards_.AccessAtCore(i)->allocated_and_unused_.load(
+                    std::memory_order_relaxed);
+    }
+    return total;
+}
+
+char *ConcurrentNVMArena::AllocateFallback(size_t bytes, bool aligned)
+{
+    if (bytes > kBlockSize / 4) {
+        ++irregular_block_num;
+        // Object is more than a quarter of our block size.  Allocate it separately
+        // to avoid wasting too much space in leftover bytes.
+        return AllocateNewBlock(bytes);
+    }
+
+    // We waste the remaining space in the current block.
+    size_t size = 0;
+    char* block_head = nullptr;
+#ifdef MAP_HUGETLB
+    if (hugetlb_size_) {
+        size = hugetlb_size_;
+        block_head = AllocateFromHugePage(size);
+    }
+#else
+    size = kBlockSize;
+    block_head = AllocateNewBlock(size);
+#endif
+    alloc_bytes_remaining_ = size - bytes;
+
+    if (aligned) {
+        aligned_alloc_ptr_ = block_head + bytes;
+        unaligned_alloc_ptr_ = block_head + size;
+        return block_head;
+    } else {
+        aligned_alloc_ptr_ = block_head;
+        unaligned_alloc_ptr_ = block_head + size - bytes;
+        return unaligned_alloc_ptr_;
+    }
+}
+
+char *ConcurrentNVMArena::AllocateNewBlock(size_t block_bytes)
+{
+    // Reserve space in `blocks_` before allocating memory via new.
+    // Use `emplace_back()` instead of `reserve()` to let std::vector manage its
+    // own memory and do fewer reallocations.
+    //
+    // - If `emplace_back` throws, no memory leaks because we haven't called `new`
+    //   yet.
+    // - If `new` throws, no memory leaks because the vector will be cleaned up
+    //   via RAII.
+    blocks_.emplace_back(nullptr);
+
+    char* block = new char[block_bytes];
+    size_t allocated_size;
+#ifdef ROCKSDB_MALLOC_USABLE_SIZE
+    allocated_size = malloc_usable_size(block);
+#ifndef NDEBUG
+    // It's hard to predict what malloc_usable_size() returns.
+    // A callback can allow users to change the costed size.
+    std::pair<size_t*, size_t*> pair(&allocated_size, &block_bytes);
+    TEST_SYNC_POINT_CALLBACK("Arena::AllocateNewBlock:0", &pair);
+#endif  // NDEBUG
+#else
+    allocated_size = block_bytes;
+#endif  // ROCKSDB_MALLOC_USABLE_SIZE
+    blocks_memory_ += allocated_size;
+    if (tracker_ != nullptr) {
+        tracker_->Allocate(allocated_size);
+    }
+    blocks_.back() = block;
+    return block;
+}
+
+void ConcurrentNVMArena::Fixup() {
+    arena_allocated_and_unused_.store(arena_.AllocatedAndUnused(),
+                                      std::memory_order_relaxed);
+    memory_allocated_bytes_.store(arena_.MemoryAllocatedBytes(),
+                                  std::memory_order_relaxed);
+    irregular_block_num_.store(arena_.IrregularBlockNum(),
+                               std::memory_order_relaxed);
+}
+
+char *ConcurrentNVMArena::Allocate(size_t bytes)
+{
+    // The semantics of what to return are a bit messy if we allow
+    // 0-byte allocations, so we disallow them here (we don't need
+    // them for our internal use).
+    assert(bytes > 0);
+    if (bytes <= alloc_bytes_remaining_) {
+        unaligned_alloc_ptr_ -= bytes;
+        alloc_bytes_remaining_ -= bytes;
+        return unaligned_alloc_ptr_;
+    }
+    return AllocateFallback(bytes, false /* unaligned */);
+}
+
+char *ConcurrentNVMArena::AllocateAligned(size_t bytes, size_t huge_page_size, Logger *logger)
+{
+    size_t rounded_up = ((bytes - 1) | (sizeof(void*) - 1)) + 1;
+    assert(rounded_up >= bytes && rounded_up < bytes + sizeof(void*) &&
+           (rounded_up % sizeof(void*)) == 0);
+
+    const int align = sizeof(void*);
+    size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align-1);
+    size_t slop = (current_mod == 0 ? 0 : align - current_mod);
+    size_t needed = bytes + slop;
+    char* result;
+    if (needed <= alloc_bytes_remaining_) {
+        result = alloc_ptr_ + slop;
+        alloc_ptr_ += needed;
+        alloc_bytes_remaining_ -= needed;
+    } else {
+        // AllocateFallback always returned aligned memory
+        result = AllocateFallback(bytes);
+    }
+    assert((reinterpret_cast<uintptr_t>(result) & (align-1)) == 0);
+    return result;
+}
+
+
+
+ConcurrentNVMArena::Shard *ConcurrentNVMArena::Repick()
+{
+    auto shard_and_index = shards_.AccessElementAndIndex();
+#ifdef ROCKSDB_SUPPORT_THREAD_LOCAL
+    // even if we are cpu 0, use a non-zero tls_cpuid so we can tell we
+    // have repicked
+    tls_cpuid = shard_and_index.second | shards_.Size();
+#endif
+    return shard_and_index.first;
+}
+
+
+
+}  // namespace rocksdb
--- nvmarena.h
+++ nvmarena.h
@@ -17,160 +17,39 @@
 #endif  // __clang__
 
 namespace rocksdb {
+
 class NVMArena : public Allocator
 {
 public:
-    // No copying allowed
-    NVMArena(const NVMArena&) = delete;
-    void operator=(const NVMArena&) = delete;
-    explicit NVMArena(size_t block_size = Arena::kMinBlockSize,
-                      AllocTracker* tracker = nullptr, size_t huge_page_size = 0);
-    ~NVMArena();
+  // No copying allowed
+  NVMArena(const NVMArena&) = delete;
+  void operator=(const NVMArena&) = delete;
+  explicit NVMArena(size_t block_size = Arena::kMinBlockSize,
+                    AllocTracker* tracker = nullptr, size_t huge_page_size = 0);
 
-    char* Allocate(size_t bytes) override;
-    char* AllocateAligned(size_t bytes, size_t huge_page_size = 0,
-                          Logger* logger = nullptr) override;
+  char* Allocate(size_t bytes) override;
+  char* AllocateAligned(size_t bytes, size_t huge_page_size = 0,
+                        Logger* logger = nullptr) override;
 
-    size_t MemoryAllocatedBytes() const {
-        return memory_allocated_bytes_.load(std::memory_order_relaxed);
-    }
-    size_t AllocatedAndUnused() const {
-        return arena_allocated_and_unused_.load(std::memory_order_relaxed) +
-                ShardAllocatedAndUnused();
-    }
-    size_t IrregularBlockNum() const {
-        return irregular_block_num_.load(std::memory_order_relaxed);
-    }
-    char *AllocateNVMBlock(size_t block_bytes);
-    size_t BlockSize() const override { return kBlockSize; }
-    Shard* Repick();
 
-    size_t ShardAllocatedAndUnused() const;
-    template <typename Func>
-    char* AllocateImpl(size_t bytes, bool force_arena, const Func& func) {
-        size_t cpu;
-        // Go directly to the arena if the allocation is too large, or if
-        // we've never needed to Repick() and the arena mutex is available
-        // with no waiting.  This keeps the fragmentation penalty of
-        // concurrency zero unless it might actually confer an advantage.
-        std::unique_lock<SpinMutex> arena_lock(arena_mutex_, std::defer_lock);
-        if (bytes > shard_block_size_ / 4 || force_arena ||
-                ((cpu = tls_cpuid) == 0 &&
-                 !shards_.AccessAtCore(0)->allocated_and_unused_.load(
-                     std::memory_order_relaxed) &&
-                 arena_lock.try_lock())) {
-            if (!arena_lock.owns_lock()) {
-                arena_lock.lock();
-            }
-            auto rv = func();
-            Fixup();
-            return rv;
-        }
+  char* AllocateFallback(size_t bytes, bool aligned);
+  char* AllocateNewBlock(size_t block_bytes);
 
-        // pick a shard from which to allocate
-        Shard* s = shards_.AccessAtCore(cpu & (shards_.Size() - 1));
-        if (!s->mutex.try_lock()) {
-            s = Repick();
-            s->mutex.lock();
-        }
-        std::unique_lock<SpinMutex> lock(s->mutex, std::adopt_lock);
+  // Stats for current active block.
+  // For each block, we allocate aligned memory chucks from one end and
+  // allocate unaligned memory chucks from the other end. Otherwise the
+  // memory waste for alignment will be higher if we allocate both types of
+  // memory from one direction.
+  char* unaligned_alloc_ptr_ = nullptr;
+  char* aligned_alloc_ptr_ = nullptr;
+  // How many bytes left in currently active block?
+  size_t alloc_bytes_remaining_ = 0;
 
-        size_t avail = s->allocated_and_unused_.load(std::memory_order_relaxed);
-        if (avail < bytes) {
-            // reload
-            std::lock_guard<SpinMutex> reload_lock(arena_mutex_);
-            // If the arena's current block is within a factor of 2 of the right
-            // size, we adjust our request to avoid arena waste.
-            auto exact = arena_allocated_and_unused_.load(std::memory_order_relaxed);
-            assert(exact == arena_.AllocatedAndUnused());
-            if (exact >= bytes && arena_.IsInInlineBlock()) {
-                // If we haven't exhausted arena's inline block yet, allocate from arena
-                // directly. This ensures that we'll do the first few small allocations
-                // without allocating any blocks.
-                // In particular this prevents empty memtables from using
-                // disproportionately large amount of memory: a memtable allocates on
-                // the order of 1 KB of memory when created; we wouldn't want to
-                // allocate a full arena block (typically a few megabytes) for that,
-                // especially if there are thousands of empty memtables.
-                auto rv = func();
-                Fixup();
-                return rv;
-            }
-            avail = exact >= shard_block_size_ / 2 && exact < shard_block_size_ * 2
-                    ? exact
-                    : shard_block_size_;
-            s->free_begin_ = arena_.AllocateAligned(avail);
-            Fixup();
-        }
-        s->allocated_and_unused_.store(avail - bytes, std::memory_order_relaxed);
-        char* rv;
-        if ((bytes % sizeof(void*)) == 0) {
-            // aligned allocation from the beginning
-            rv = s->free_begin_;
-            s->free_begin_ += bytes;
-        } else {
-            // unaligned from the end
-            rv = s->free_begin_ + avail - bytes;
-        }
-        return rv;
-    }
+  // Array of new[] allocated memory blocks
+  std::vector<char*> blocks_;
 
-#ifdef MAP_HUGETLB
-    size_t hugetlb_size_ = 0;
-#endif  // MAP_HUGETLB
-    char* AllocateFromHugePage(size_t bytes);
-    char* AllocateFallback(size_t bytes, bool aligned);
-    char* AllocateNewBlock(size_t block_bytes);
-
-    void Fixup();
-
-    struct Shard {
-        char padding[40] ROCKSDB_FIELD_UNUSED;
-        mutable SpinMutex mutex;
-        char* free_begin_;
-        std::atomic<size_t> allocated_and_unused_;
-
-        Shard() : free_begin_(nullptr), allocated_and_unused_(0) {}
-    };
-
-#ifdef ROCKSDB_SUPPORT_THREAD_LOCAL
-    static __thread size_t tls_cpuid;
-#else
-    enum ZeroFirstEnum : size_t { tls_cpuid = 0 };
-#endif
-
-    char padding0[56] ROCKSDB_FIELD_UNUSED;
-
-    size_t shard_block_size_;
-
-    CoreLocalArray<Shard> shards_;
-
-    Arena arena_;
-    mutable SpinMutex arena_mutex_;
-    std::atomic<size_t> arena_allocated_and_unused_;
-    std::atomic<size_t> memory_allocated_bytes_;
-    std::atomic<size_t> irregular_block_num_;
-
-    std::string mfile;
-    const size_t kBlockSize;
-
-    size_t irregular_block_num = 0;
-    // Stats for current active block.
-    // For each block, we allocate aligned memory chucks from one end and
-    // allocate unaligned memory chucks from the other end. Otherwise the
-    // memory waste for alignment will be higher if we allocate both types of
-    // memory from one direction.
-    char* unaligned_alloc_ptr_ = nullptr;
-    char* aligned_alloc_ptr_ = nullptr;
-    // How many bytes left in currently active block?
-    size_t alloc_bytes_remaining_ = 0;
+  size_t blocks_memory_ = 0;
+  AllocTracker* tracker_;
 };
-
-
-
-inline char* NVMArena::Allocate(size_t bytes);
-
-inline char* AllocateAlligned(size_t bytes, size_t huge_page_size = 0,
-                              Logger* logger = nullptr);
 
 }  // namespace rocksdb
